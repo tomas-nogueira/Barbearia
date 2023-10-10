7458f2874e4e73c05ec2fbda1ba2b287e4fe3715
tomas-nogueira <tomasmnogueira2006@gmail.com>
2023-10-10 14:19:48 -0300

    Primeira parte realizada

---
+++ Models/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Barbearia.Models
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options) : base(options) 
+        { 
+            
+        }
+    }
+}
